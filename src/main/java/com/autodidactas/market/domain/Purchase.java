77e7d86702b792068d76eba368fe65675ccb411c
Bruno Roman Huaman <bruno.roman@unmsm.edu.pe>
2023-01-18 20:19:21 -0500

    Adicion del dominio compras

---
+++ src/main/java/com/autodidactas/market/domain/Purchase.java
@@ -0,0 +1,9 @@
+package com.autodidactas.market.domain;
+
+
+public class Purchase {
+
+
+
+
+}
--- src/main/java/com/autodidactas/market/web/controller/ProductController.java
+++ src/main/java/com/autodidactas/market/web/controller/ProductController.java
@@ -3,10 +3,13 @@ package com.autodidactas.market.web.controller;
 import com.autodidactas.market.domain.Product;
 import com.autodidactas.market.domain.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 @RestController
 @RequestMapping("/products")
@@ -16,26 +19,32 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping()
-    public List<Product> getAll() {
-        return productService.getAll();
+    public ResponseEntity<List<Product>> getAll() {
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Optional<Product> getProduct(@PathVariable("id") Long productId) {
-        return productService.getProduct(productId);
+    public ResponseEntity<Product> getProduct(@PathVariable("id") Long productId) {
+        return productService.getProduct(productId)
+                .map(product -> new ResponseEntity<>(product,HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
+
     @GetMapping("/category/{id}")
-    public Optional<List<Product>> getByCategory(@PathVariable("id") Long categoryId) {
-        return productService.getByCategory(categoryId);
+    public ResponseEntity<List<Product>> getByCategory(@PathVariable("id") Long categoryId) {
+            return productService.getByCategory(categoryId).filter(Predicate.not(List::isEmpty))
+                    .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
+                    .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping()
-    public Product save(@RequestBody Product product) {
-        return productService.save(product);
+    public ResponseEntity<Product> save(@RequestBody Product product) {
+        return new ResponseEntity<>(productService.save(product),HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public boolean delete(@PathVariable("id") Long productId){
-        return productService.delete(productId);
+    public ResponseEntity delete(@PathVariable("id") Long productId){
+        return productService.delete(productId) ? new ResponseEntity<>(HttpStatus.OK): new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+
 }
